93a478b9ddb5feaada22fcc1849600bdbede6ee0
Vuthekiet <142762737+Vuthekiet@users.noreply.github.com>
2026-08-11 11:48:43 +0700

    Create sever.js

---
+++ sever.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Bokietvidai.html'));
+});
+
+app.listen(PORT, () => console.log(`Server Trái Tim 3D chạy tại port ${PORT}`));
