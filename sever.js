6b9ac6be98de42afc573368ea6663f4dd75d5753
Vuthekiet <142762737+Vuthekiet@users.noreply.github.com>
2026-08-11 11:50:46 +0700

    Update sever.js

--- sever.js
+++ sever.js
@@ -3,10 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Phục vụ các file tĩnh trong thư mục
 app.use(express.static(__dirname));
 
+// Đọc trực tiếp file Bokietvidai.html làm trang chủ
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Bokietvidai.html'));
+    res.sendFile(path.join(__dirname, 'Bokietvidai.html'));
 });
 
-app.listen(PORT, () => console.log(`Server Trái Tim 3D chạy tại port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server đang chạy tại port ${PORT}`);
+});
